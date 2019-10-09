657ab51676d46dc5f66f0cd549cea9c222764426
Sanath <ksanath1997@gmail.com>
2019-10-10 02:45:37 +0530

    Dashboard Updated, moved public folder things to src and deleted public

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LogIn from './Login';
+import Homepage from './HomePage.js'
+//import Login from './Login';
 import * as serviceWorker from './serviceWorker';
+import HomePage from "./HomePage";
+import Transfer from "./Transfer";
+import Donation from "./Donation";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// function App() {
+
+//     return (
+//         <div>
+//           <Route exact path="/login" component={Login} />
+//           <Route exact path="/home" component={HomePage} />
+//         </div>
+//     );
+
+// }
+
+
+ReactDOM.render(<HomePage/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
